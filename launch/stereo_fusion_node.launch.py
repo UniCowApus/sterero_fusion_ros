bee9b2807cdd5af977cbbf4768e1479f1d9bec2c
David Rogers <david.c.rogers488@gmail.com>
2026-01-11 14:51:25 -0500

    add log level launch args, and respawn

--- launch/stereo_fusion_node.launch.py
+++ launch/stereo_fusion_node.launch.py
@@ -1,12 +1,27 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
+    log_level = LaunchConfiguration('log_level')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'log_level',
+            default_value='info',
+            description='Logging level'
+        ),
+
         Node(
             package='stereo_fusion_node',
             executable='stereo_fusion_node',
             name='stereo_fusion',
-            output='screen'
+            output='screen',
+            arguments=['--ros-args', '--log-level', log_level],
+            respawn=True,
+            respawn_delay=2.0,
         )
     ])
+
