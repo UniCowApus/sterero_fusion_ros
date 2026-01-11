f2fae73c6887d052648e1f51e116860b310e5db9
David Rogers <david.c.rogers488@gmail.com>
2026-01-11 15:12:58 -0500

    add readme and final polish

--- include/stereo_fusion_node/stereo_fusion_node.hpp
+++ include/stereo_fusion_node/stereo_fusion_node.hpp
@@ -1,77 +1,106 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
+
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+
+#include <visualization_msgs/msg/marker_array.hpp>
+
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
+
 #include <cv_bridge/cv_bridge.h>
 #include <image_geometry/pinhole_camera_model.h>
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
-#include "visualization_msgs/msg/marker_array.hpp"
-#include <sensor_msgs/msg/compressed_image.hpp>
 
 struct Detection
 {
-  int class_id;
-  float confidence;
-  cv::Rect box;
+    int class_id;
+    float confidence;
+    cv::Rect box;
 };
 
 struct Detection3D
 {
-  Detection det;
-  cv::Point3f position_cam;
-  float disparity;
+    Detection det;
+    cv::Point3f position_cam;
+    float disparity;
 };
 
 class StereoFusionNode : public rclcpp::Node
 {
 public:
-  StereoFusionNode();
+    StereoFusionNode();
 
 private:
-  void leftInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
-  void rightInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
-  void stereoCallback(const sensor_msgs::msg::CompressedImage::ConstSharedPtr &left_msg,
-                      const sensor_msgs::msg::CompressedImage::ConstSharedPtr &right_msg);
-  bool rectifyStereoPair(const sensor_msgs::msg::CompressedImage::ConstSharedPtr &left_msg,
-                         const sensor_msgs::msg::CompressedImage::ConstSharedPtr &right_msg,
-                         cv::Mat &left_rectified,
-                         cv::Mat &right_rectified);
-  bool computeDisparitySGBM(
-      const cv::Mat &left_rect,
-      const cv::Mat &right_rect,
-      cv::Mat &disparity);
-  std::vector<Detection> detect(const cv::Mat &image, int input_width_ = 640, int input_height_ = 640, float conf_thresh_ = 0.4f, float nms_thresh_ = 0.5f);
-  bool estimateDepthFromDisparity(
-      const std::vector<Detection> &detections,
-      const cv::Mat &disparity,
-      std::vector<Detection3D> &out_detections_3d);
-  void loadONNX();
+    void leftInfoCallback(
+        const sensor_msgs::msg::CameraInfo::SharedPtr msg);
 
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_image_pub_;
-  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr disparity_pub_;
-  message_filters::Subscriber<sensor_msgs::msg::CompressedImage> left_image_sub_;
-  message_filters::Subscriber<sensor_msgs::msg::CompressedImage> right_image_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr left_info_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr right_info_sub_;
+    void rightInfoCallback(
+        const sensor_msgs::msg::CameraInfo::SharedPtr msg);
 
-  using SyncPolicy =
-      message_filters::sync_policies::ApproximateTime<
-          sensor_msgs::msg::CompressedImage,
-          sensor_msgs::msg::CompressedImage>;
-  using Synchronizer = message_filters::Synchronizer<SyncPolicy>;
-  std::shared_ptr<Synchronizer> sync_;
+    void stereoCallback(
+        const sensor_msgs::msg::CompressedImage::ConstSharedPtr &left_msg,
+        const sensor_msgs::msg::CompressedImage::ConstSharedPtr &right_msg);
 
-  image_geometry::PinholeCameraModel left_cam_model_;
-  image_geometry::PinholeCameraModel right_cam_model_;
+    bool rectifyStereoPair(
+        const sensor_msgs::msg::CompressedImage::ConstSharedPtr &left_msg,
+        const sensor_msgs::msg::CompressedImage::ConstSharedPtr &right_msg,
+        cv::Mat &left_rectified,
+        cv::Mat &right_rectified);
 
-  bool left_info_received_ = false;
-  bool right_info_received_ = false;
-  cv::dnn::Net net_;
+    bool computeDisparitySGBM(
+        const cv::Mat &left_rect,
+        const cv::Mat &right_rect,
+        cv::Mat &disparity);
+
+    std::vector<Detection> detect(
+        const cv::Mat &image,
+        int input_width_ = 640,
+        int input_height_ = 640,
+        float conf_thresh_ = 0.4f,
+        float nms_thresh_ = 0.5f);
+
+    bool estimateDepthFromDisparity(
+        const std::vector<Detection> &detections,
+        const cv::Mat &disparity,
+        std::vector<Detection3D> &out_detections_3d);
+
+    void loadONNX();
+
+private:
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_image_pub_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr disparity_pub_;
+
+    message_filters::Subscriber<sensor_msgs::msg::CompressedImage> left_image_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::CompressedImage> right_image_sub_;
+
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr left_info_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr right_info_sub_;
+
+    using SyncPolicy =
+        message_filters::sync_policies::ApproximateTime<
+            sensor_msgs::msg::CompressedImage,
+            sensor_msgs::msg::CompressedImage>;
+
+    using Synchronizer =
+        message_filters::Synchronizer<SyncPolicy>;
+
+    std::shared_ptr<Synchronizer> sync_;
+
+    image_geometry::PinholeCameraModel left_cam_model_;
+    image_geometry::PinholeCameraModel right_cam_model_;
+
+    bool left_info_received_ = false;
+    bool right_info_received_ = false;
+
+    cv::dnn::Net net_;
 };
